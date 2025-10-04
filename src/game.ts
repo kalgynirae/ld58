78278d393bf32b0d747819ad6d91d3287825d399
Colin Chan <colin+git@lumeh.org>
2025-10-03 22:22:30 -0700

    Start writing collision detection

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,15 @@
+class Rect {
+  constructor(public x1: number, public y1: number, public x2: number, public y2: number) { }
+
+  intersects(other: Rect): boolean {
+    return !(this.x1 > other.x2 || this.x2 < other.x1 || this.y1 > other.y2 || this.y2 < other.y1);
+  }
+
+  toString() {
+    return `Rect(${this.x1}, ${this.x2}, ${this.y1}, ${this.y2})`;
+  }
+}
+
 class Moveable {
   id: string;
   element: HTMLElement;
@@ -13,6 +25,10 @@ class Moveable {
     this.originX = Number(e.dataset.x ?? 0);
     this.originY = Number(e.dataset.y ?? 0);
     this.reset();
+  }
+
+  rect(): Rect {
+    return new Rect(this.posX, this.posY, this.posX + this.element.offsetWidth, this.posY + this.element.offsetHeight);
   }
 
   reset() {
@@ -34,14 +50,14 @@ class Moveable {
 }
 
 const gameWindow = document.querySelector(".game-window") as HTMLElement;
-let moveables: { [id: string]: Moveable } = {};
+let moveables: Map<string, Moveable> = new Map();
 
 {
   let next_id = 1;
   document.querySelectorAll(".moveable").forEach((element) => {
     let m = new Moveable(String(next_id++), element as HTMLElement);
     m.reset();
-    moveables[m.id] = m;
+    moveables.set(m.id, m);
   });
 }
 
@@ -52,11 +68,14 @@ let moveables: { [id: string]: Moveable } = {};
 
   document.addEventListener("mousedown", (event: MouseEvent) => {
     const e = event.target as HTMLElement;
-    if (e.dataset.id && e.dataset.id in moveables) {
-      active = moveables[e.dataset.id];
+    if (e.dataset.id && moveables.has(e.dataset.id)) {
+      active = moveables.get(e.dataset.id)!;
       offsetX = event.pageX - active.posX;
       offsetY = event.pageY - active.posY;
-      console.log(`active=${active.id} offsetX=${offsetX} offsetY=${offsetY}`);
+      // console.log(`active=${active.id} offsetX=${offsetX} offsetY=${offsetY}`);
+      // for (let m of moveables.values()) {
+      //   console.log(`id=${m.id}  rect=${m.rect()}`);
+      // }
     }
   });
 
@@ -68,7 +87,16 @@ let moveables: { [id: string]: Moveable } = {};
 
   document.addEventListener("mousemove", (event: MouseEvent) => {
     if (active != null) {
-      active.moveTo(event.pageX - offsetX!, event.pageY - offsetY!);
+      let x = event.pageX - offsetX!;
+      let y = event.pageY - offsetY!;
+      for (let m of moveables.values()) {
+        if (m === active) continue;
+        if (m.rect().intersects(active.rect())) {
+          console.log(`intersects with ${m.id}`);
+          // TODO: prevent movement into intersecting position
+        }
+      }
+      active.moveTo(x, y);
     }
   });
 }

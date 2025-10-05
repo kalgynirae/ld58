9f3b67d6c87d62476ad58ea33b978aff8d994b0b
Colin Chan <colin+git@lumeh.org>
2025-10-05 14:03:58 -0700

    Add a placeholder music file

---
+++ src/audio.ts
@@ -0,0 +1,26 @@
+const context = new AudioContext();
+let music: AudioBuffer | null = null;
+let yoink: AudioBuffer | null = null;
+
+async function load(path: string): Promise<AudioBuffer> {
+  const response = await fetch(path);
+  const buffer = await response.arrayBuffer();
+  return await context.decodeAudioData(buffer);
+}
+
+async function loadMusicAndSounds() {
+  music = await load("audio/music.mp3");
+  yoink = await load("audio/yoink.mp3");
+}
+
+function startMusic() {
+  const gainNode = context.createGain();
+  gainNode.gain.value = 0.5;
+  gainNode.connect(context.destination);
+  const musicNode = new AudioBufferSourceNode(context, {
+    buffer: music,
+    loop: true,
+  });
+  musicNode.connect(gainNode);
+  musicNode.start();
+}
